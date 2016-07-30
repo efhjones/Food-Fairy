4195fc77e4b8f1c2ae4f82c7ae54703f030eb69e
Emily Jones <efhjones@gmail.com>
2016-07-30 17:50:23 -0600

    Adds successful API request and render, needs RecipeListEntry rendered

--- client/containers/RecipeListEntry.js
+++ client/containers/RecipeListEntry.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import RecipeList from './RecipeList';
+
 // RecipeEntry receives each recipe from the RecipeList (array) component and renders a small (image + description + likes) or a large card (instructions)
-export default class RecipeList extends React.Component {
+export default class RecipeListEntry extends React.Component {
   //props: recipe - the recipe data to be shown; taken from the API
   constructor(props){
     super(props);
@@ -15,15 +17,22 @@ export default class RecipeList extends React.Component {
     this.props.setRecipe(id);
   }
 
-
   render(){
+    console.log('Recipe list entry', this.props);
     return (
-      <div className="recipe-list-entry"
-        onClick={() => this.onRecipeClick(recipe.id)}>
-       {this.props.recipe}
+      <div>YOYOYOYOYO
+         {this.props.recipe}
+         <div>
+          <img src={this.props.recipe.image} />
+         </div>
+         <div>
+          {this.props.recipe.title}
+        </div>
       </div>
     )
   }
 }
+
+
 
 
--- client/main.js
+++ client/main.js
@@ -5,9 +5,10 @@ import { Router, Route, Link, hashHistory, IndexRoute} from 'react-router';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import routes from './Router';
+import Promise_Handler from './middleware/promise_Handler';
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(Promise_Handler)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
